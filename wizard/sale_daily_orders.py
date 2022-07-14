2463bc833119cedfc438d3afe5b4f0e09178c713
powerfulman86 <dba.ahmedmaher@gmail.com>
2022-07-14 15:55:05 +0200

    UPDate

--- wizard/sale_daily_orders.py
+++ wizard/sale_daily_orders.py
@@ -127,6 +127,7 @@ class SaleDailyOrders(models.TransientModel):
                 'sale_contract': so.order_id.sale_contract.internal_reference,
             }
             result.append(res)
+        result.sort(key=lambda b: b['delivery_receipt_number'])
 
         datas = {
             'ids': self,
